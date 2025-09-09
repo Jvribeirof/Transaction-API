c66381d3800bec301e3d9f119ec3689b742aa370
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2025-09-09 20:04:19 -0300

    Creating an UserDetails Storage System in RBDs

---
+++ src/main/java/com/unibanco/itau/JpaUserDetailService.java
@@ -0,0 +1,21 @@
+package com.unibanco.itau;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailService implements UserDetailsService {
+    private final UsersRepository usersRepository;
+
+    public JpaUserDetailService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("[Exception by me] User not found"));
+    }
+}
--- src/main/java/com/unibanco/itau/SecurityConfig.java
+++ src/main/java/com/unibanco/itau/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.unibanco.itau;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 @EnableWebSecurity
@@ -24,28 +28,37 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/health").permitAll()
-                        .requestMatchers(HttpMethod.GET).authenticated()
-                        .anyRequest().hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CommandLineRunner initDatabase(UsersRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if(userRepository.findByUsername("user").isEmpty()){
+                var password = passwordEncoder.encode("123");
+                var username = "user";
+                var role = List.of("USER");
+                Users user = new Users(username, password, role);
+                userRepository.save(user);
+            }
+            if(userRepository.findByUsername("admin").isEmpty()){
+                var password = passwordEncoder.encode("123");
+                var username = "admin";
+                var role = List.of("USER", "ADMIN");
+                Users admin = new Users(username, password, role);
+                userRepository.save(admin);
+            }
+        };
+    }
+
 }
