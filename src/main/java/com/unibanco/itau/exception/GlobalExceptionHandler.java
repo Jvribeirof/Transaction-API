faedb83e5982978ed2510221ae809c9f1070b18e
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2025-09-18 20:20:40 -0300

    Applying Auth Exception Handling

--- src/main/java/com/unibanco/itau/exception/GlobalExceptionHandler.java
+++ src/main/java/com/unibanco/itau/exception/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.unibanco.itau.exception;
 
 import com.unibanco.itau.controller.TransactionController;
+import io.jsonwebtoken.JwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +25,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Void> genericHandler(Exception e) {
         logger.info(e.getMessage());
         return ResponseEntity.internalServerError().build();
+    }
+
+    @ExceptionHandler({
+            AuthenticationException.class,
+            BadCredentialsException.class,
+            CredentialsExpiredException.class,
+            AuthenticationCredentialsNotFoundException.class,
+            JwtException.class})
+    public ResponseEntity<String> handlerAuthentication(Exception e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed: " + e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handlerAccessDenied(Exception e) {
+        return ResponseEntity.status(403).body("Authorization failed: " + e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
